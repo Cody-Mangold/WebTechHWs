72aa57e06862b2cd166da9a992440f04d19ce91e
Cody-Mangold <124631149+Cody-Mangold@users.noreply.github.com>
2024-06-11 10:08:06 -0500

    Add files via upload

---
+++ hw17/school.php
@@ -0,0 +1,37 @@
+
+
+    
+
+
+     <!-- ABOUT -->
+     <section id="about">
+          <div class="container">
+               <div class="row">
+
+                    <div class="col-md-6 col-sm-12">
+                         <div class="about-info">
+                              <h2>Cody's School Information!</h2>
+
+						<div class="school-info">
+      					<img src="https://cdn.pixabay.com/photo/2017/08/06/22/01/books-2596809_1280.jpg" alt="Free from Pixabay" class="square-img">
+     						 <p>I am a Computer Science Student at UTSA. I am in my last semester currently, and will graduate this semester if everything goes well. My current Schedule is:</p>
+    					</div>
+ 				 </div>
+
+  <div class="course-list">
+    <ol>
+      <li><i>User interfaces</i></li>
+      <li><i>Web Technologies</i></li>
+      <li><i>Programming Languages</i></li>
+      <li><i>Mathematical optimization and applied mathematical topics</i></li>
+    </ol>
+  </div>
+                         </div>
+                    </div>
+					
+               </div>
+          
+     </section>
+
+
+    
---
+++ hw18/home.php
@@ -0,0 +1,93 @@
+   <!-- HOME -->
+     <section id="home">
+          <div class="row">
+
+                    <div class="owl-carousel owl-theme home-slider">
+                         <div class="item item-first">
+                              <div class="caption">
+                                   <div class="container">
+                                        <div class="col-md-6 col-sm-12">
+                                             <h1>Distance Learning Education Center</h1>
+                                             <h3>Our online courses are designed to fit in your industry supporting all-round with latest technologies.</h3>
+                                             <a href="#feature" class="section-btn btn btn-default smoothScroll">Discover more</a>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+
+                         <div class="item item-second">
+                              <div class="caption">
+                                   <div class="container">
+                                        <div class="col-md-6 col-sm-12">
+                                             <h1>Start your journey with our practical courses</h1>
+                                             <h3>Our online courses are built in partnership with technology leaders and are designed to meet industry demands.</h3>
+                                             <a href="#courses" class="section-btn btn btn-default smoothScroll">Take a course</a>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+
+                         <div class="item item-third">
+                              <div class="caption">
+                                   <div class="container">
+                                        <div class="col-md-6 col-sm-12">
+                                             <h1>Efficient Learning Methods</h1>
+                                             <h3>Nam eget sapien vel nibh euismod vulputate in vel nibh. Quisque eu ex eu urna venenatis sollicitudin ut at libero. Visit <a href="https://plus.google.com/+templatemo" target="_parent">templatemo</a> page.</h3>
+                                             <a href="#contact" class="section-btn btn btn-default smoothScroll">Let's chat</a>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+                    </div>
+          </div>
+     </section>
+
+     <!-- ABOUT -->
+     <section id="about">
+          <div class="container">
+               <div class="row">
+
+                    <div class="col-md-6 col-sm-12">
+                         <div class="about-info">
+                              <h2>My name is Cody Mangold and this is my first Websites Home Page. I hope it is informative.</h2>
+
+                              <figure>
+                                   <span><i class="fa fa-users"></i></span>
+                                   <figcaption>
+                                        <h3>My Work Info!</h3>
+                                        <p>Check out my work tab for more information about my work history!</p>
+                                   </figcaption>
+                              </figure>
+
+                              <figure>
+                                   <span><i class="fa fa-certificate"></i></span>
+                                   <figcaption>
+                                        <h3>Hobbies!</h3>
+                                        <p>Check out my hobbies tab for all the details! I love anime/manga and video games!</p>
+                                   </figcaption>
+                              </figure>
+
+                              <figure>
+                                   <span><i class="fa fa-bar-chart-o"></i></span>
+                                   <figcaption>
+                                        <h3>School!</h3>
+                                        <p>Checkout my school tab for my current course load and more about my current experience at UTSA!</p>
+                                   </figcaption>
+                              </figure>
+							  <figure>
+                                   <span><i class="fa fa-certificate"></i></span>
+                                   <figcaption>
+                                        <h3>Contact!</h3>
+                                        <p>More to come soon!</p>
+                                   </figcaption>
+                              </figure>
+                         </div>
+                    </div>
+
+                    <div class="col-md-offset-1 col-md-4 col-sm-12">
+                       <img src="/images/Webtech-photo.jpg" alt="My personal photo taken at the CS lab" class="rounded-img">
+                    </div>
+
+               </div>
+          </div>
+     </section>
